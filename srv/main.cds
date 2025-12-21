8f4112c119517837e94d962bec764d1ae773fd38
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-21 12:56:57 -0300

    feat: add authorizations in ts code

--- srv/main.cds
+++ srv/main.cds
@@ -2,10 +2,6 @@
 
 // traz o pacote inteiro que está no namespace sales
 using { sales } from '../db/schema';
-
-@requires: 'admin'
-service MainService{
     entity SalesOrderHeaders as projection on sales.SalesOrderHeaders;
     entity Customers as projection on sales.Customers;
     entity Products as projection on sales.Products;
-}
--- srv/main.ts
+++ srv/main.ts
@@ -2,6 +2,18 @@ import cds, { Request, Service } from '@sap/cds';
 import { Customers, Products, SalesOrderHeader, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
+    service.before('READ', '*', (request: Request) => {
+        if(!request.user.is('read_only_user')){
+            return request.reject(403, 'Não autorizado');
+        }
+    });
+
+    service.before(['WRITE', 'DELETE'], '*', (request: Request) => {
+        if(!request.user.is('read_only_user')){
+            return request.reject(403, 'Não autorizada a escrita/deleção');
+        }
+    });
+
     service.after('READ', 'Customers', (results: Customers) => {
         results.forEach(customer => {
             if (!customer.email?.includes('@')){
