9cf4ca0d3ef3ab2d8ad4f61d8fabcc45c4f0c0d7
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-23 09:10:56 -0300

    feat: add new dependency for salesOrderService factory

--- srv/factories/services/sales-order-header.ts
+++ srv/factories/services/sales-order-header.ts
@@ -2,11 +2,13 @@ import { SalesOrderHeaderService } from "srv/services/sales-order-header/protoco
 import {SalesOrderHeaderServiceImpl} from "srv/services/sales-order-header/implementation";
 import { CustomerRepositoryImpl } from "srv/repositories/customer/implementation";
 import { ProductRepositoryImpl } from "srv/repositories/product/implementation";
+import { SalesOrderLogRepositoryImpl } from "srv/repositories/sales-order-log/implementation";
 
 const makeSalesOrderHeaderService = (): SalesOrderHeaderService => {
     const customerRepository = new CustomerRepositoryImpl();
     const productRepository = new ProductRepositoryImpl();
-    return new SalesOrderHeaderServiceImpl(customerRepository, productRepository);
+    const salesOrderLogRepository = new SalesOrderLogRepositoryImpl();
+    return new SalesOrderHeaderServiceImpl(customerRepository, productRepository, salesOrderLogRepository);
 }
 
 export const salesOrderHeaderService = makeSalesOrderHeaderService();
