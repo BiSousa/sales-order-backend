280950cea15b40dcc93e333320ed1e350b9a61c1
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-24 12:41:31 -0300

    feat: add return result with header in bulk create action

--- srv/services/sales-order-header/protocols.ts
+++ srv/services/sales-order-header/protocols.ts
@@ -1,5 +1,6 @@
 import { CustomerModel } from '@/models/customer';
 import { ProductModel } from '@/models/product';
+import { SalesOrderHeaderModel } from '@/models/sales-order-header';
 import { User } from '@cds-models';
 
 import { Payload as BuckCreateSalesOrderPayload } from '@models/db/types/BulkCreateSalesOrder';
@@ -11,6 +12,7 @@ export type CreationPayloadValidationResult = {
     products?: ProductModel[];
     customer?: CustomerModel;
     error?: Error;
+    headers?: BuckCreateSalesOrderPayload[];
 };
 
 export interface SalesOrderHeaderService {
