0d384cbe02a12670620c108209308c9cc96c4aa9
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-24 13:35:01 -0300

    feat: add cloneSalesOrder method in SalesOrderHeaderController and SalesOrderHeaderControllerImpl

--- srv/controllers/sales-order-header/implementation.ts
+++ srv/controllers/sales-order-header/implementation.ts
@@ -25,4 +25,8 @@ export class SalesOrderHeaderControllerImpl implements SalesOrderHeaderControlle
     ): Promise<CreationPayloadValidationResult> {
         return this.service.bulkCreate(headers, loggedUser);
     }
+
+    public async cloneSalesOrder(id: string, loggedUser: User): Promise<CreationPayloadValidationResult> {
+        return this.service.cloneSalesOrder(id, loggedUser);
+    }
 }
