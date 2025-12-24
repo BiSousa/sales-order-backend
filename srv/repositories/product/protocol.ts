8a35f475b7ed1316442decf178841724f19ec2b6
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-24 12:24:24 -0300

    feat: create SalesOrderHeaderRepository and SalesOrderHeaderRepositoryImpl

--- srv/repositories/product/protocol.ts
+++
@@ -1,6 +0,0 @@
-import { ProductModel, ProductProps } from '@/models/product';
-
-export interface ProductRepository {
-    findByIds(ids: ProductProps['id'][]): Promise<ProductModel[] | null>;
-    updateStock(product: ProductModel): Promise<void>;
-}
