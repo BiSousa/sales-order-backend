0bebf012e05b94c75f3f1cbca94dd471159cfb1b
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-27 15:05:29 -0300

    feat: add BaseController

---
+++ srv/controllers/base/implementation.ts
@@ -0,0 +1,16 @@
+import { BaseController, BaseControllerResponse } from '@/controllers/base/protocols';
+
+export class BaseControllerImpl implements BaseController {
+    public success(data: unknown): BaseControllerResponse {
+        return {
+            data,
+            status: 200
+        };
+    }
+    public error(code: number, message: string): BaseControllerResponse {
+        return {
+            status: code,
+            data: message
+        };
+    }
+}
