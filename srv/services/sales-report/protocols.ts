351e85471f627f694eda19d01d707d19ee1eacfe
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-27 15:26:00 -0300

    feat: add Either Monad approach in SalesReportService

--- srv/services/sales-report/protocols.ts
+++ srv/services/sales-report/protocols.ts
@@ -1,6 +1,8 @@
+import { AbstractError } from '@/errors';
+import { Either } from '@sweet-monads/either';
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 
 export interface SalesReportService {
-    findByDays(days: number): Promise<SalesReportByDays[]>;
+    findByDays(days: number): Promise<Either<AbstractError, SalesReportByDays[]>>;
     findByCustomerId(customerId: string): Promise<SalesReportByDays[]>;
 }
