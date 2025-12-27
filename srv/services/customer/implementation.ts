4af4eb2d97ae2e754158c0ffd6aa72cea31f0243
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-27 15:04:56 -0300

    feat: use either monad approach in CustomerService

--- srv/services/customer/implementation.ts
+++ srv/services/customer/implementation.ts
@@ -1,19 +1,26 @@
 import { CustomerModel } from '@/models/customer';
 import { CustomerService } from '@/services/customer/protocols';
 import { Customers } from '@models/sales';
+import { left } from '@sweet-monads/either';
+import { AbstractError, ServerError } from '@/errors';
 
 export class CustomerServiceImpl implements CustomerService {
-    public afterRead(customerList: Customers): Customers {
-        // transforma o CustomerList para ser do modelo
-        const customers = customerList.map((c) => {
-            const customer = CustomerModel.with({
-                id: c.id as string,
-                firstName: c.firstName as string,
-                lastName: c.lastName as string,
-                email: c.email as string
+    public afterRead(customerList: Customers): Either<AbstractError, Customers> {
+        try {
+            // transforma o CustomerList para ser do modelo
+            const customers = customerList.map((c) => {
+                const customer = CustomerModel.with({
+                    id: c.id as string,
+                    firstName: c.firstName as string,
+                    lastName: c.lastName as string,
+                    email: c.email as string
+                });
+                return customer.setDefaultEmailDomain().toObject();
             });
-            return customer.setDefaultEmailDomain().toObject();
-        });
-        return customers;
+            return right(customers);
+        } catch (error) {
+            const errorInstance: Error = error as Error;
+            return left(new ServerError(errorInstance.stack as string, errorInstance.message));
+        }
     }
 }
