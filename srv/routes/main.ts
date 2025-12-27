a9666a17c5b63b7b07c55cf1050ff4e8891770ce
Beatriz Carvalho Sousa <biacsousa72@gmail.com>
2025-12-27 15:06:37 -0300

    feat: implement either monad approach in cap service

--- srv/routes/main.ts
+++ srv/routes/main.ts
@@ -24,7 +24,11 @@ export default (service: Service) => {
     });
 
     service.after('READ', 'Customers', (customersList: Customers, request) => {
-        (request as unknown as FullRequestParams<Customers>).results = customerController.afterRead(customersList);
+        const result = customerController.afterRead(customersList);
+        if (result.status >= 400) {
+            return request.error(request.status, request.data as string);
+        }
+        (request as unknown as FullRequestParams<Customers>).results = result.data as Customers;
     });
 
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
